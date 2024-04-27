f74e2a0a6cf0396c40a40701ca3129b303d03c42
palash <palash@spoilz.studio>
2024-04-27 19:51:53 +0530

    TwoSum done

---
+++ Easy/TwoSum.js
@@ -0,0 +1,50 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+/********************************* ANSWER ***********************************/
+
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+    let dict = {};
+    let rem = 0;
+    let ans = [];
+  
+    for(let i=0; i<array.length; i++)
+    {
+      rem = targetSum - array[i];
+      if(dict[rem])
+      {
+        ans.push(rem, array[i]);
+      }
+      else
+      {
+        dict[array[i]] = 1;
+      }
+    }
+    return ans;
+  }
+  
+  // Do not edit the line below.
+  exports.twoNumberSum = twoNumberSum;
+  
